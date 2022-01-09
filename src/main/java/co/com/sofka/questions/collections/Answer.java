a84e13f11969dac12d23229a5233ab219158acc8
Cristian Cabarcas <cridejecato@gmail.com>
2022-01-08 21:47:48 -0500

    Cadena de conexión para despliegue

--- src/main/java/co/com/sofka/questions/collections/Answer.java
+++ src/main/java/co/com/sofka/questions/collections/Answer.java
@@ -3,7 +3,7 @@ package co.com.sofka.questions.collections;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "Respuestas")
 public class Answer {
     @Id
     private String id;
